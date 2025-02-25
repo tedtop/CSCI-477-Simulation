cd5994ff61837fc54c813d19fece004487380047
tedtop <tedtoporkov@gmail.com>
2025-02-25 09:54:42 -0700

    Added RungeKutta to ode_solver

--- ode_solver.py
+++ ode_solver.py
@@ -14,27 +14,72 @@ def EulerRichardson(dt, f, t, y, args):
     y_next = y + f(t + dt / 2, y_mid, *args) * dt  # Second step: update using the midpoint
     return y_next
 
+def RungeKutta(dt, f, t, y, args):
+    """
+    h: time step (dt)
+    f: function to integrate
+    t: current time
+    y: current state
+    args: additional arguments to f
+    """
+    k1 = f(t, y, *args)
+    k2 = f(t + dt / 2, y + k1 * dt / 2, *args)
+    k3 = f(t + dt / 2, y + k2 * dt / 2, *args)
+    k4 = f(t + dt, y + k3 * dt, *args)
+
+    return y + (k1 + 2 * k2 + 2 * k3 + k4) * dt / 6
+
 def solve_ode(f, tspan, y0, method=Euler, *args, **options):
     """
     Given a function f that returns derivatives,
-        dy / dt = f(t, y)
-    and an initial state:
-        y(tspan[0]) = y0
+    dy / dt = f(t, y)
+    and an inital state:
+    y(tspan[0]) = y0
+
     This function will return the set of intermediate states of y
-    from t0 (tspan[0]) to tf (tspan[1]).
+    from t0 (tspan[0]) to tf (tspan[1])
 
-    INPUTS:
-    f - function handle that returns derivatives of y at time t.
-        Can accept additional parameters passed via *args.
-    tspan - iterable containing [t0, tf], initial and final times.
-    y0 - Initial state of the system (numpy array).
-    method - Integration method (Euler, Euler-Cromer, or Euler-Richardson).
-    *args - Additional parameters for function f.
-    **options - Optional keyword arguments:
-        first_step - Initial time step (default: 0.01).
 
-    OUTPUTS:
-    t, y - Numpy arrays of time steps and corresponding y values.
+
+    The function is called as follows:
+
+    INPUTS
+
+    f - the function handle to the function that returns derivatives of the
+        vector y at time t. The function can also accept parameters that are
+        passed via *args, eg f(t,y,g) could accept the acceleration due to gravity.
+
+    tspan - a indexed data type that has [t0 tf] as its two members.
+            t0 is the initial time
+            tf is the final time
+
+    y0 - The initial state of the system, must be passed as a numpy array.
+
+    method - The method of integrating the ODEs. This week will be one of Euler,
+             Euler-Cromer, or Euler-Richardson
+
+    *args - a tuple containing as many additional parameters as you would like for
+            the function handle f.
+
+    **options - a dictionary containing all the keywords that might be used to control
+                function behavior. For now, there is only one:
+
+                first_step - the initial time step for the simulation.
+
+
+    OUTPUTS
+
+    t,y
+
+    The returned states will be in the form of a numpy array
+    t containing the times the ODEs were solved at and an array
+    y with shape tsteps,N_y where tsteps is the number of steps
+    and N_y is the number of equations. Observe this makes plotting simple:
+
+    plt.plot(t,y[:,0])
+
+    would plot positions.
+
     """
     t0, tf = tspan[0], tspan[1]
     dt = options.get("first_step", 0.01)
@@ -44,7 +89,7 @@ def solve_ode(f, tspan, y0, method=Euler, *args, **options):
 
     while t[-1] < tf:
         current_t, current_y = t[-1], y[-1]
-        next_y = method(dt, f, current_t, current_y, *args)
+        next_y = method(dt, f, current_t, current_y, *args) # this shouldn't unpack args (*)
 
         y.append(next_y)
         t.append(current_t + dt)
